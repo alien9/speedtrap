e7ae6fb1e27562a66a32a3ef3b997a0e0ed34202
Tiago Barufi <barufi@gmail.com>
2020-08-28 10:49:33 -0300

    testa se arquivo ja existe

--- collect_counter.py
+++ collect_counter.py
@@ -3,44 +3,44 @@
 
 #465520180201210	9
 
-import sys, re, psycopg2, csv
+import os, sys, re, psycopg2, csv
 from config import cstring
 conn = psycopg2.connect(cstring)
 cur = conn.cursor()
 
-cur.execute("select nextval('contagens_id_seq'::regclass)")
-vid=cur.fetchone()[0]
+if not os.path.exists("contagens.csv"):
+    cur.execute("select nextval('contagens_id_seq'::regclass)")
+    vid=cur.fetchone()[0]
+    file_contagens=open("contagens.csv", "w+", newline='')
+    writer_contagens=csv.writer(file_contagens)
+    n=0
 
-file_contagens=open("contagens.csv", "w+", newline='')
-writer_contagens=csv.writer(file_contagens)
-n=0
+    for line in sys.stdin:
+        line = line.strip()
 
-for line in sys.stdin:
-    line = line.strip()
-
-    m = re.search('(?P<local>\d{4})(?P<faixa>\d{1})(?P<ano>\d{4})(?P<mes>\d{2})(?P<dia>\d{2})(?P<hora>\d{2})(?P<tipo>\d{1})\t(?P<contagem>\d+)\t(?P<autuacoes>\d+)\t(?P<placas>\d+)', line)
-    try:
-        writer_contagens.writerow([
-            vid,
-            m.group("local"),
-            m.group("faixa"),
-            m.group("tipo"),
-            m.group("contagem"),
-            m.group("autuacoes"),
-            m.group("placas"),
-            "%s-%s-%s %s:00:00" % (m.group("ano"),m.group("mes"),m.group("dia"),m.group("hora"))
-        ])
-        vid+=1
-    except Exception as x:
-        print(str(x))
-        print("registro mal formado")
-        exit();
-    if re.search("00000$", str(n)):
-        print("\n"+str(n))
-    n+=1
+        m = re.search('(?P<local>\d{4})(?P<faixa>\d{1})(?P<ano>\d{4})(?P<mes>\d{2})(?P<dia>\d{2})(?P<hora>\d{2})(?P<tipo>\d{1})\t(?P<contagem>\d+)\t(?P<autuacoes>\d+)\t(?P<placas>\d+)', line)
+        try:
+            writer_contagens.writerow([
+                vid,
+                m.group("local"),
+                m.group("faixa"),
+                m.group("tipo"),
+                m.group("contagem"),
+                m.group("autuacoes"),
+                m.group("placas"),
+                "%s-%s-%s %s:00:00" % (m.group("ano"),m.group("mes"),m.group("dia"),m.group("hora"))
+            ])
+            vid+=1
+        except Exception as x:
+            print(str(x))
+            print("registro mal formado")
+            exit();
+        if re.search("00000$", str(n)):
+            print("\n"+str(n))
+        n+=1
 
 
-file_contagens.close()
+    file_contagens.close()
 with open("contagens.csv", 'r') as f:
     cur.copy_from(f, 'contagens', sep=',')
 
--- collect_trip.py
+++ collect_trip.py
@@ -4,23 +4,6 @@
 import sys, re, psycopg2, datetime, csv
 from config import cstring
 
-next_viagem=1
-next_trajeto=1
-
-
-conn = psycopg2.connect(cstring)
-cur = conn.cursor()
-
-cur.execute("select nextval('trajetos_id_seq'::regclass)")
-next_trajeto=cur.fetchone()[0]
-cur.execute("select nextval('viagens_id_seq'::regclass)")
-next_viagem=cur.fetchone()[0]
-
-file_viagens=open("viagens.csv", "w+", newline='')
-file_trajetos=open("trajetos.csv", "w+", newline='')
-writer_trajetos=csv.writer(file_trajetos)
-writer_viagens=csv.writer(file_viagens)
-
 def ponto(t):
     m = re.search('(?P<local>\d{4})(?P<ano>\d{4})(?P<mes>\d{2})(?P<dia>\d{2})(?P<hora>\d{2})(?P<minuto>\d{2})(?P<segundo>\d{2})(?P<tipo>\d{1})(?P<velocidade>\d{3})', t)
     try:
@@ -65,52 +48,72 @@ def viaja(pts):
 
 def filtro(p):
     return p!=None
-n=0
-nv=0
-for line in sys.stdin:
-    line = line.strip()
-    if re.match('\w+\t\w+', line):
-        nv+=1
-        placa, trip = re.split('\t', line)
-        #print(placa, end="\r")
-        pontos = list(filter(filtro, re.split('\|', trip)))
-        pts=list(filter(filtro, map(ponto, pontos)))
-        pts.sort(key=data_e_hora)
-        trips=viaja(pts)
-        if len(trips)>0:
-            #print(trips)
-            n+=len(trips)
-            for trip in trips:
-                writer_viagens.writerow([
-                    next_viagem,
-                    trip[0]['local'],
-                    trip[0]['data_e_hora'].isoformat(),
-                    trip[len(trip)-1]['local'],
-                    trip[len(trip)-1]['data_e_hora'].isoformat(),
-                    trip[0]['tipo']
-                ])
-                i=1
-                while i<len(trip):
-                    writer_trajetos.writerow([
-                        next_trajeto,
-                        trip[i - 1]['tipo'],
-                        trip[i - 1]['data_e_hora'].isoformat(),
-                        trip[i]['data_e_hora'].isoformat(),
-                        trip[i - 1]['local'],
-                        trip[i]['local'],
-                        next_viagem,
-                        trip[i - 1]['velocidade'],
-                        trip[i]['velocidade']
-                    ])
-                    next_trajeto+=1
-                    #print(trip[i])
-                    i+=1
-                next_viagem+=1
 
-#conn.commit()
-print("%s veículos encontrados." % (nv,))
-print("%s pontos detectados em %s viagens." % (p,trs))
-print("%s viagens válidas encontradas." % (n,))
+conn = psycopg2.connect(cstring)
+cur = conn.cursor()
+
+if not os.path.exists("viagens.csv") or not os.path.exists("trajetos.csv"):
+    next_viagem=1
+    next_trajeto=1
+
+
+
+    cur.execute("select nextval('trajetos_id_seq'::regclass)")
+    next_trajeto=cur.fetchone()[0]
+    cur.execute("select nextval('viagens_id_seq'::regclass)")
+    next_viagem=cur.fetchone()[0]
+
+    file_viagens=open("viagens.csv", "w+", newline='')
+    file_trajetos=open("trajetos.csv", "w+", newline='')
+    writer_trajetos=csv.writer(file_trajetos)
+    writer_viagens=csv.writer(file_viagens)
+
+    n=0
+    nv=0
+    for line in sys.stdin:
+        line = line.strip()
+        if re.match('\w+\t\w+', line):
+            nv+=1
+            placa, trip = re.split('\t', line)
+            #print(placa, end="\r")
+            pontos = list(filter(filtro, re.split('\|', trip)))
+            pts=list(filter(filtro, map(ponto, pontos)))
+            pts.sort(key=data_e_hora)
+            trips=viaja(pts)
+            if len(trips)>0:
+                #print(trips)
+                n+=len(trips)
+                for trip in trips:
+                    writer_viagens.writerow([
+                        next_viagem,
+                        trip[0]['local'],
+                        trip[0]['data_e_hora'].isoformat(),
+                        trip[len(trip)-1]['local'],
+                        trip[len(trip)-1]['data_e_hora'].isoformat(),
+                        trip[0]['tipo']
+                    ])
+                    i=1
+                    while i<len(trip):
+                        writer_trajetos.writerow([
+                            next_trajeto,
+                            trip[i - 1]['tipo'],
+                            trip[i - 1]['data_e_hora'].isoformat(),
+                            trip[i]['data_e_hora'].isoformat(),
+                            trip[i - 1]['local'],
+                            trip[i]['local'],
+                            next_viagem,
+                            trip[i - 1]['velocidade'],
+                            trip[i]['velocidade']
+                        ])
+                        next_trajeto+=1
+                        #print(trip[i])
+                        i+=1
+                    next_viagem+=1
+
+    #conn.commit()
+    print("%s veículos encontrados." % (nv,))
+    print("%s pontos detectados em %s viagens." % (p,trs))
+    print("%s viagens válidas encontradas." % (n,))
 with open("trajetos.csv", 'r') as f:
     cur.copy_from(f, 'trajetos', sep=',')
 with open("viagens.csv", 'r') as f:
